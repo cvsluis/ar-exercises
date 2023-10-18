6d08f3d7ae554a8b3a5a64b38d7638b3f5c4be1d
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-10-18 14:30:17 -0700

    feat: add validations for models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+
+puts store.errors.full_messages
+
+# Errors Displayed: 
+# Name is too short (minimum is 3 characters)
+# Annual revenue is not a number
+# Either mens or womens apparel must be carried at this store
